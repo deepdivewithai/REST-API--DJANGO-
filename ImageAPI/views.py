6473b674b5c6e27e42b2741ac84fd7ff6d5f2109
Chetan <chetan.aiofficial@gmail.com>
2023-09-03 13:58:37 +0530

    Model Initialize

--- ImageAPI/views.py
+++ ImageAPI/views.py
@@ -3,6 +3,14 @@ from rest_framework import generics
 from ImageAPI.models import Image
 from .serializers import ImageSerializer
 
+from keras.preprocessing import image
+from keras.models import load_model
+import tensorflow as tf
+
+import numpy as np
+import json
+import math
+
 class ImageListView(generics.ListCreateAPIView):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
@@ -10,3 +18,16 @@ class ImageListView(generics.ListCreateAPIView):
 class ImageDetailView(generics.RetrieveDestroyAPIView):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
+
+
+with open('./models/imagenet_classes.json', 'r') as f:
+    labelInfo = f.read()
+
+labelInfo = json.loads(labelInfo)
+
+
+model_graph = tf.Graph()
+with model_graph.as_default():
+    tf_session = tf.compat.v1.Session()
+    with tf_session.as_default():
+        model = load_model('./models/MobileNetModelImagenet.h5') 
