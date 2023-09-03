d97dfeb3ad1deb62ef513c3460518475d1e9e270
Chetan <chetan.aiofficial@gmail.com>
2023-09-03 11:48:36 +0530

    Docker

--- Images/settings.py
+++ Images/settings.py
@@ -110,6 +110,12 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
